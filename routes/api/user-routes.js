6460d9652fefa6b84e9b89b4d54348db439bc4dd
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-10 14:10:48 -0500

    login authentication

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -57,6 +57,26 @@ router.post('/', (req, res) => {
       });
   });
 
+  router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });  
+    });
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
@@ -76,7 +96,7 @@ router.put('/:id', (req, res) => {
           res.status(404).json({ message: 'No user found with this id' });
           return;
         }
-        res.json(dbUserData);
+        //res.json(dbUserData);
       })
       .catch(err => {
         console.log(err);
